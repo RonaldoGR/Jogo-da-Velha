099cea03baccc6e3293e30c043eb0fb997ac9262
Ronaldo Gandra Rocha <ronaldogandrarocha@gmail.com>
2024-06-08 12:07:34 -0300

    criado estados no GameOption

--- react-jogo-da-velha/src/components/game/Game.jsx
+++ react-jogo-da-velha/src/components/game/Game.jsx
@@ -1,9 +1,13 @@
 import styles from './Game.module.css'
 
 import GameOption from '../gameoption/GameOption'
+import { useState } from 'react'
 
 
 function Game () {
+    const [gameState,setGameState] = useState(Array(9).fill(0))
+    console.log(gameState)
+
   return (
     <div className={styles.game}>
       <GameOption />
