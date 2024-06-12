6485e015f29e874c38dddd06c14ccc1a5bf97ce8
Ronaldo Gandra Rocha <ronaldogandrarocha@gmail.com>
2024-06-12 14:55:31 -0300

    criado o empate

--- react-jogo-da-velha/src/components/game/Game.jsx
+++ react-jogo-da-velha/src/components/game/Game.jsx
@@ -23,6 +23,7 @@ function Game () {
     const [currentPlayer,setCurrentPlayer] = useState(-1)
     const [winner, setWinner] = useState(0)
     const [winnerLine,setWinnerLine] = useState([])
+    const [draw,setDraw] = useState(false)
 
 
     const handleClick = (pos) => {
@@ -50,6 +51,13 @@ function Game () {
       setGameState(Array(9).fill(0)) 
       setWinner(0)
       setWinnerLine([])
+      setDraw(false)
+    }
+
+    const verifyDraw = () => {
+      if (gameState.find((value)=> value === 0 ) === undefined && winner === 0){
+        setDraw(true)
+      }
     }
 
     const verifyWinnerLine = (pos) => 
@@ -58,7 +66,12 @@ function Game () {
     useEffect(() => {
       setCurrentPlayer(currentPlayer * -1)
       verifyGame()
+      verifyDraw()
     }, [gameState])
+
+   useEffect (() => {
+    if (winner !== 0) setDraw(false)
+   }, [winner]) 
 
   return (
     <div className={styles.gameContent}>
@@ -70,7 +83,7 @@ function Game () {
         status = {value}
         onClick={() => handleClick(pos)}
         isWinner = {verifyWinnerLine(pos)}
-        
+        isDraw = {draw}
         />
         )
       }
@@ -80,6 +93,7 @@ function Game () {
     currentPlayer = {currentPlayer}
     winner = {winner}
     onReset = {handleReset}
+    isDraw = {draw}
     />
     
   </div>
--- react-jogo-da-velha/src/components/gameinfo/GameInfo.jsx
+++ react-jogo-da-velha/src/components/gameinfo/GameInfo.jsx
@@ -2,15 +2,20 @@ import Icon from '../icon/Icon'
 import styles from './GameInfo.module.css'
 import Button from '../button/Button'
 
-function GameInfo ({currentPlayer, winner, onReset}) {
-  const shouldEnableButton = () => {
+function GameInfo ({currentPlayer, winner, onReset, isDraw}) {
+  const shouldEnableButton = () => winner !== 0 || isDraw
+
+    /*
     if (winner !== 0) return true
-  }
+    if (isDraw) return true
+    quando se tem duas condições que só retorna true pode ser colocado tbm do jeito acima.
+    */
+  
 
   return (
     <div className={styles.gameInfo}>
       {
-         winner === 0 && 
+         !isDraw && winner === 0 && 
          <>
           <h4>Próximo a jogar:</h4>
          {
@@ -22,7 +27,7 @@ function GameInfo ({currentPlayer, winner, onReset}) {
          </>        
       }
       {
-        winner !== 0 && 
+        !isDraw && winner !== 0 && 
           <>  
               <h4>Fim de jogo! Campeão: </h4>
               {
@@ -34,6 +39,9 @@ function GameInfo ({currentPlayer, winner, onReset}) {
               
               }
          </>
+      }
+      {
+         isDraw && <h4>Empate!</h4>
       }
       <Button
           onClick = {onReset}
